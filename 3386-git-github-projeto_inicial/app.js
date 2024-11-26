271a3495e234b8d5ee0047ba455c7bd678b553bf
JenniferReetz <jenniferheloisareetz@gmail.com>
2024-11-26 18:33:20 -0300

    Ajustando a função para o topo

--- 3386-git-github-projeto_inicial/app.js
+++ 3386-git-github-projeto_inicial/app.js
@@ -4,6 +4,7 @@ let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
 exibirMensagemInicial();
+
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
@@ -14,7 +15,6 @@ function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
-
 function verificarChute() {
     let chute = document.querySelector('input').value;
     
