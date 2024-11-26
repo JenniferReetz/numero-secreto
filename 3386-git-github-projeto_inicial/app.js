094d607236aa315c79680ed98662c0ee44d75a50
JenniferReetz <jenniferheloisareetz@gmail.com>
2024-11-26 18:28:09 -0300

    Teste inicial

--- 3386-git-github-projeto_inicial/app.js
+++ 3386-git-github-projeto_inicial/app.js
@@ -3,6 +3,7 @@ let numeroLimite = 40;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
+exibirMensagemInicial();
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
@@ -13,8 +14,6 @@ function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
-
-exibirMensagemInicial();
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
